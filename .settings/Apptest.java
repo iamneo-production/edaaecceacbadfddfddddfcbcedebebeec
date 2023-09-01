048f70a933195611f3364c8f58dc1ba2f82ecc0a
Iamneo <techadmin@iamneo.ai>
2023-09-01 07:09:13 +0000

    Backup Commit

--- .settings/Apptest.java
+++ .settings/Apptest.java
@@ -17,28 +17,29 @@ public class Apptest {
 
               sign_in_button.click();
  
-driver.switchTo().frame("loginIframe");
-// Locate the email input field, enter a valid email, and click Continue
-WebElement email_input = driver.findElement(By.id("userName"));
-email_input.sendKeys("your_valid_email@example.com");
-WebElement continue_button = driver.findElement(By.id("checkUser"));
-continue_button.click();
-// Locate the password input field, enter a valid password, and click LOGIN
-WebElement password_input = driver.findElement(By.id("j_password_login_uc"));
-password_input.sendKeys("your_valid_password");
-WebElement login_button = driver.findElement(By.id("submitLoginUC"));
-login_button.click();
-// Verify that the user is logged in successfully by checking a specific element on the logged-in page
-WebElement success_element = driver.findElement(By.xpath("//span[contains(text(), 'Welcome')]"));
-if (success_element != null) {
-System.out.println("Login successful!");
-} else {
-System.out.println("Login failed!");
+              driver.switchTo().frame("loginIframe");
+ 
+              WebElement email_input = driver.findElement(By.id("userName"));
+              email_input.sendKeys("your_valid_email@example.com");
+              WebElement continue_button = driver.findElement(By.id("checkUser"));
+              continue_button.click();
+              WebElement password_input = driver.findElement(By.id("j_password_login_uc"));
+              password_input.sendKeys("your_valid_password");
+              WebElement login_button = driver.findElement(By.id("submitLoginUC"));
+              login_button.click();
+              WebElement success_element = driver.findElement(By.xpath("//span[contains(text(), 'Welcome')]"));
+              if (success_element != null) {
+                  System.out.println("Login successful!");
+              } else {
+                  System.out.println("Login failed!");
+              } 
+            
+            } finally {
+
+                driver.quit();
+            }
+        }
+    }
 }
-} finally {
-// Close the browser
-driver.quit();
-}
-}
-}
+
 
